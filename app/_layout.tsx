7513e2d4a31a891465790323bd31fe10d2b6a7d8
Diln Lemos <dilan.lemos@correounivalle.edu.co>
2025-10-10 15:15:18 -0500

    Ajuste de Barra de Navegacion e Iconos

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,72 @@
 import { Tabs } from 'expo-router';
+import { Image, StyleSheet } from 'react-native';
 
 export default function TabLayout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="Training" />
-      <Tabs.Screen name="Sleep" />
-      <Tabs.Screen name="food" />
-      <Tabs.Screen name="Stats" />
+      // Barra de Navegación
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          height: 70,
+          paddingTop: 20,
+        },
+      }}
+    >
+      <Tabs.Screen // Botón de Entreno
+        name="Training"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Image
+              source={require('../assets/images/TrainingIcon.png')}
+              style={[styles.icon, { tintColor: color }]}
+            />
+          ),
+        }}
+      />
+
+      <Tabs.Screen // Botón de mimir
+        name="Sleep"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Image
+              source={require('../assets/images/SleepIcon.png')}
+              style={[styles.icon, { tintColor: color }]}
+            />
+          ),
+        }}
+      />
+
+      <Tabs.Screen // Botón de alimentación
+        name="food"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Image
+              source={require('../assets/images/EatIcon.png')}
+              style={[styles.icon, { tintColor: color }]}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen // Botón de stats
+        name="Stats"
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Image
+              source={require('../assets/images/DumbbellIcon.png')}
+              style={[styles.icon, { tintColor: color }]}
+            />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
+
+// Estilo de iconos
+const styles = StyleSheet.create({
+  icon: {
+    width: 28,
+    height: 28,
+  },
+});
